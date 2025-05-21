291c3ce767d6034dea35e8bfea3809512fd88486
averin.ilya <averin.ilya@inbox.ru>
2025-05-21 21:33:49 +0500

    fix

--- src/CarDriver.cpp
+++ src/CarDriver.cpp
@@ -7,9 +7,9 @@
 #define SERVO_PIN 13  // GPIO13 (PWM1, пин 33)  сюда подключен сигнальный провод от сервопривода
 #define ENGINE_PIN 12  // GPIO12 (PWM1, пин 32) сюда подключен сигнальный провод от двигателя
 
-#define ENGINE_FORWARD 1570  // значение сигнала вперед (мск)
+#define ENGINE_FORWARD 1580  // значение сигнала вперед (мск)
 #define ENGINE_STOP 1500  // значение сигнала стоп (мск)
-#define ENGINE_BACKWARD 1410  // значение сигнала назад (мск)
+#define ENGINE_BACKWARD 1400  // значение сигнала назад (мск)
 
 #define SERVO_LEFT 1900  // значение сигнала влево (мск)  (-70°)
 #define SERVO_NONE 1600  // значение сигнала прямо (0°)
